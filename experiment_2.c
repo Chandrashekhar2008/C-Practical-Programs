6c098a312a1d4a88e753e98c0a725f6dd1b7ca6e
Chandrashekhar2008 <chandrashekhar11208@gmail.com>
2025-12-30 19:43:44 +0530

    Experiment 2 added

--- experiment_2.c
+++ experiment_2.c
@@ -6,12 +6,36 @@ Experiment Name: Program to print name and age using printf()
 Name: Pukka Chandrashekhar
 ERP ID: RU-25-11057
 --------------------------------------------------
+
+Aim:
+To write a C program to print name and age using printf() function.
+
+Algorithm:
+1. Start
+2. Use printf() to display name
+3. Use printf() to display age
+4. End
+
+Flowchart:
+   Start
+     |
+ Print Name
+     |
+ Print Age
+     |
+    End
 */
 
 #include <stdio.h>
 
 int main() {
     printf("Name: Pukka Chandrashekhar\n");
-    printf("Age: 17\n");
+    printf("Age: 18\n");
     return 0;
 }
+
+/*
+Output:
+Name: Pukka Chandrashekhar
+Age: 18
+*/
