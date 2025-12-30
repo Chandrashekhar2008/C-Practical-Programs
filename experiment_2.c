9d0654a51a20fd91dafce69a6aad18b9d7ab1f9f
Chandrashekhar2008 <chandrashekhar11208@gmail.com>
2025-12-30 19:33:39 +0530

    Experiment 2 added

---
+++ experiment_2.c
@@ -0,0 +1,17 @@
+/*
+--------------------------------------------------
+Experiment No: 2
+Experiment Name: Program to print name and age using printf()
+
+Name: Pukka Chandrashekhar
+ERP ID: RU-25-11057
+--------------------------------------------------
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Name: Pukka Chandrashekhar\n");
+    printf("Age: 17\n");
+    return 0;
+}
