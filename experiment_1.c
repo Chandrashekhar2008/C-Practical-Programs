955d5f9df351f96ab91651f4907814834a2895d5
Chandrashekhar2008 <chandrashekhar11208@gmail.com>
2025-12-30 19:26:03 +0530

    Experiment 1 added
    Initial C program for experiment documentation.

---
+++ experiment_1.c
@@ -0,0 +1,17 @@
+/*
+--------------------------------------------------
+Experiment No: 1
+Experiment Name: Installation of VS Code & GCC and execution of first C program
+
+Name: Pukka Chandrashekhar
+ERP ID: RU-25-11057
+--------------------------------------------------
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("C Practical Programs Repository Created Successfully.\n");
+    return 0;
+}
